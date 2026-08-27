68bca6980f8f6cf6577318ee2003d8b87e2f868d
WolfY_D3v <181145610+WolfYD3v@users.noreply.github.com>
2026-08-27 14:02:44 +0200

    PWA Setup Done

--- sw.js
+++ sw.js
@@ -1,24 +1,26 @@
-const CACHE_NAME = "v3";
+const CURRENT_CACHE_NAME = "v1";
+const ALLOWED_CACHES = ["v1"];
 const FILES_TO_CACHE = [
     "/",
+    "/sw.js",
     "/index.html",
-    "/js/app.js",
-    "/assets/icons/icon.png",
-    "/style/style.css",
     "/manifest.json",
-    "/sw.js"
+    "/assets/icons/base_icon.png",
+    "/assets/icons/icon512_maskable.png",
+    "/assets/icons/icon512_rounded.png",
+    "/style/style.css",
+    "/js/app.js"
 ]
 
 self.addEventListener("install", event => {
     console.log("Service Worker Installed !");
     event.waitUntil(
-        caches.open(CACHE_NAME)
+        caches.open(CURRENT_CACHE_NAME)
             .then(cache => cache.addAll(FILES_TO_CACHE))
     )
 })
 self.addEventListener("activate", event => {
     console.log("Service Worker Activated !");
-    const ALLOWED_CACHES = ["v3"];
     event.waitUntil(
         caches.keys().then(keys => Promise.all(
             keys.map(key => {
@@ -30,7 +32,7 @@ self.addEventListener("activate", event => {
     );
     self.skipWaiting();
 })
-self.addEventListener("fetch", event => { event.respondWith(
+self.addEventListener("fetch", event => {event.respondWith(
     caches.match(event.request)
         .then(response => response || fetch(event.request))
-)})
+    )})
