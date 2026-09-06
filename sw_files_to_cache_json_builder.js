48ba0bed3dbb98e9ddb82bc26918d4a0a68c70cd
WolfY_D3v <181145610+WolfYD3v@users.noreply.github.com>
2026-09-06 11:41:09 +0200

    Final Update

--- sw_files_to_cache_json_builder.js
+++ sw_files_to_cache_json_builder.js
@@ -51,7 +51,7 @@ function createJSONFile(files = []) {
 
 
 let directoriesIgnoreList = [".git", ".sass-cache", ".vscode", "figma", "node_modules"];
-let filesIgnoreList = [swFilesToCacheJSON, "test_gradient.html", "ANALYSE.txt", "CREDITS.txt", "sw_files_to_cache_json_builder.js", "LICENSE", ".scss", "package", ".md", ".sh", ".map", "questions_de_reflexion_reponses.txt"];
+let filesIgnoreList = [swFilesToCacheJSON, "test_gradient.html", "ANALYSE.txt", "CREDITS.txt", "sw_files_to_cache_json_builder.js", "LICENSE", ".scss", "package", ".md", ".sh", ".map", ".pdf", "questions_de_reflexion_reponses.txt"];
 let ignoreList = [...directoriesIgnoreList, ...filesIgnoreList];
 createJSONFile(
     listFilesForJSON(".", ignoreList)
