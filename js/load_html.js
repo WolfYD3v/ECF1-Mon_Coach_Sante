960d8484e18d4e7c66780a0128ae08305ead2f32
WolfY_D3v <181145610+WolfYD3v@users.noreply.github.com>
2026-09-06 09:38:06 +0200

    HTML Loader Updated

--- js/load_html.js
+++ js/load_html.js
@@ -3,8 +3,11 @@ export function loadHTML(HTMLFilePath, elementId) {
 
     return fetch(HTMLFilePath)
         .then(response => response.text())
-        .then(text => {
-            let container = document.getElementById(elementId);
-            if (container) { container.insertAdjacentHTML('beforeend', text); }
-        });
+        .then(text => tryInsertHTML(elementId, text))
+        .catch(err => console.error(`HTML Loader - The Loading of the HTML File '${HTMLFilePath}' (for Element with Id '${elementId}') Failed: ${err}`));
+}
+
+function tryInsertHTML(elementId, newHTML) {
+    let container = document.getElementById(elementId);
+    if (container) { container.insertAdjacentHTML('beforeend', newHTML); }
 }
